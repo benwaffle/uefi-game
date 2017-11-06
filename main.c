304b3f3cd27f7de65ab7eabab592ac752c74573b
Ben Iofel <iofelben@gmail.com>
2017-11-05 20:06:49 -0500

    read key presses

--- main.c
+++ main.c
@@ -3,7 +3,26 @@
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE img, EFI_SYSTEM_TABLE *systab) {
     InitializeLib(img, systab);
-    Print(L"Hello, world\n");
+    Print(L"Hello, world!\r\nwaiting for keypress \r\n");
+
+    EFI_STATUS res = EFI_SUCCESS;
+    while (res == EFI_SUCCESS) {
+        UINTN index = -1;
+        res = uefi_call_wrapper(systab->BootServices->WaitForEvent, 3, 1, &systab->ConIn->WaitForKey, &index);
+        if (EFI_ERROR(res)) {
+            Print(L"error: %d\n", res);
+        } else {
+            EFI_INPUT_KEY key;
+            uefi_call_wrapper(systab->ConIn->ReadKeyStroke, 2, systab->ConIn, &key);
+            Print(L"%c", key.UnicodeChar);
+
+            if (key.UnicodeChar == 'q') {
+              break;
+            }
+        }
+    }
+
+    Print(L"\r\n");
 
     return EFI_SUCCESS;
 }
