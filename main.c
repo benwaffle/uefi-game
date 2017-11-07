2db0c42d81cba78ef13e1958b3cfac64f89b1d2e
Ben Iofel <iofelben@gmail.com>
2017-11-07 01:37:25 -0500

    just stick with built-in resolution

--- main.c
+++ main.c
@@ -14,70 +14,33 @@
   } \
 }
 
-static int memcmp(const void *s1, const void *s2, UINTN n)
-{
-	const unsigned char *c1 = s1, *c2 = s2;
-	int d = 0;
-
-	if (!s1 && !s2)
-		return 0;
-	if (s1 && !s2)
-		return 1;
-	if (!s1 && s2)
-		return -1;
-
-	while (n--) {
-		d = (int)*c1++ - (int)*c2++;
-		if (d)
-			break;
-	}
-	return d;
+void memset(void *s, int c, unsigned n) {
+  UINT8 *b = s;
+  for (int i = 0; i < n; ++i)
+    *(b++) = c;
 }
-
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE img, EFI_SYSTEM_TABLE *systab) {
   InitializeLib(img, systab);
-  Print(L"Hello, world!\r\n");
+  Print(L"This is a game\r\n");
 
   EFI_GRAPHICS_OUTPUT_PROTOCOL *gop;
 
   EFI_STATUS status = LibLocateProtocol(&GraphicsOutputProtocol, (void**)&gop);
   CHECK(status, TRUE);
 
-  for (int i = 0; i < gop->Mode->MaxMode; ++i) {
-    UINTN sizeOfInfo;
-    EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *info;
-    status = uefi_call_wrapper(gop->QueryMode, 4, gop, i, &sizeOfInfo, &info);
-    CHECK(status, TRUE);
+  int width = gop->Mode->Info->HorizontalResolution;
+  int height = gop->Mode->Info->VerticalResolution;
 
-    Print(L"%c%dx%d ", memcmp(gop->Mode->Info, info, sizeof(*info)) == 0 ? '*' : ' ', info->HorizontalResolution, info->VerticalResolution);
-		switch(info->PixelFormat) {
-      case PixelRedGreenBlueReserved8BitPerColor:
-        Print(L"RGBR");
-        break;
-      case PixelBlueGreenRedReserved8BitPerColor:
-        Print(L"BGRR");
-        break;
-      case PixelBitMask:
-        Print(L"R:%08x G:%08x B:%08x X:%08x",
-              info->PixelInformation.RedMask,
-              info->PixelInformation.GreenMask,
-              info->PixelInformation.BlueMask,
-              info->PixelInformation.ReservedMask);
-        break;
-      case PixelBltOnly:
-        Print(L"(blt only)");
-        break;
-      default:
-        Print(L"(Invalid pixel format)");
-        break;
-		}
-		Print(L" pitch %d\n", info->PixelsPerScanLine);
-    WaitForSingleEvent(ST->ConIn->WaitForKey, 0);
-    EFI_INPUT_KEY key;
-    status = uefi_call_wrapper(ST->ConIn->ReadKeyStroke, 2, ST->ConIn, &key);
-    CHECK(status, FALSE);
+  if (gop->Mode->Info->PixelFormat != PixelBlueGreenRedReserved8BitPerColor) {
+    Print(L"Error: don't know about pixel format %d\r\n", gop->Mode->Info->PixelFormat);
+    Exit(EFI_SUCCESS, 0, NULL);
   }
+
+  Print(L"%dx%d\r\n", width, height);
+
+  UINT8 Buffer[width * height];
+  memset(Buffer, 0, width * height);
 
   return EFI_SUCCESS;
 }
