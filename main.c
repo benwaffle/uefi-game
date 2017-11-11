83336e58534b0c3371ae8abd52d8b4130b862cb9
Ben Iofel <iofelben@gmail.com>
2017-11-10 19:13:04 -0500

    wasd second player

--- main.c
+++ main.c
@@ -48,13 +48,16 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE img, EFI_SYSTEM_TABLE *systab) {
   uint64_t size = width * height * sizeof(EFI_GRAPHICS_OUTPUT_BLT_PIXEL);
   uint32_t *buffer = AllocateZeroPool(size);
 
-  int x = 300;
-  int y = 400;
+  int x = 200;
+  int y = 300;
+  int x2 = 400;
+  int y2 = 400;
   int delta = 40;
 
   while (TRUE) {
     square(buffer, color(0xf4, 0x71, 0x42), 0, 0, width, height, width);
     square(buffer, color(0x59, 0xf4, 0x42), x, y, 100, 100, width);
+    square(buffer, color(0, 0, 0xff), x2, y2, 100, 100, width);
 
     status = uefi_call_wrapper(gop->Blt, 10, gop, buffer, EfiBltBufferToVideo, 0, 0, 0, 0, width, height, 0);
     CHECK(status, FALSE);
@@ -67,12 +70,22 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE img, EFI_SYSTEM_TABLE *systab) {
       if (key.ScanCode == SCAN_DOWN) y += delta;
       if (key.ScanCode == SCAN_LEFT) x -= delta;
       if (key.ScanCode == SCAN_RIGHT) x += delta;
+
+      if (key.UnicodeChar == 'w') y2 -= delta;
+      if (key.UnicodeChar == 'a') x2 -= delta;
+      if (key.UnicodeChar == 's') y2 += delta;
+      if (key.UnicodeChar == 'd') x2 += delta;
     }
 
     if (y < 0) y = 0;
     if (y + 100 > height) y = height-100;
     if (x < 0) x = 0;
     if (x + 100 > width) x = width-100;
+
+    if (y2 < 0) y2 = 0;
+    if (y2 + 100 > height) y2 = height-100;
+    if (x2 < 0) x2 = 0;
+    if (x2 + 100 > width) x2 = width-100;
   }
 
   Print(L"press any key to exit...\r\n");
